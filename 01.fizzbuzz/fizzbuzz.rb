f798df76f1255052d557ae45549483b334878be5
wattun-git <michiaki.watsuda@smarthr.co.jp>
2024-04-07 12:47:19 +0900

    FizzBuzz問題のプログラムを作成

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,12 @@
+fizzbuzz = (1..20).to_a
+fizzbuzz.each do |i|
+  if i % 3 == 0 && i % 5 == 0
+    puts "FizzBuzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  elsif i % 5 == 0
+    puts "Buzz"
+  else
+    puts i
+  end
+end
