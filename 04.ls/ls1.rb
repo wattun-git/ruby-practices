7ac32ac3a62b67e5e0a2401d064648504c5f3401
wattun-git <michiaki.watsuda@smarthr.co.jp>
2024-06-09 11:31:07 +0900

    行毎のアイテム数の計算を四捨五入→切り上げに修正。変数の再代入している箇所を修正

--- 04.ls/ls1.rb
+++ 04.ls/ls1.rb
@@ -8,20 +8,20 @@ def get_files(path)
   Dir.open(path).each_child.reject { |file| file.start_with?('.') }.sort
 end
 
-def tranpose_files(files)
-  display_row = (files.size.to_f / DISPLAY_COLUMN_NUMBER).round
+def transpose_files(files)
+  display_row = (files.size.to_f / DISPLAY_COLUMN_NUMBER).ceil
   return [files] if display_row <= 1
 
-  files = files.each_slice(display_row)
-  max_size = files.map(&:size).max
-  files
+  files_sliced_by_columns = files.each_slice(display_row)
+  max_size = files_sliced_by_columns.map(&:size).max
+  files_sliced_by_columns
     .map { |file| file.fill(nil, file.size...max_size) }
     .transpose
     .each { |file| file.reject!(&:nil?) }
 end
 
-def display_files(files)
-  files.each do |files_per_row|
+def display_files(files_sliced_by_rows)
+  files_sliced_by_rows.each do |files_per_row|
     files_per_row.each do |file|
       print file.ljust(TERMINAL_WIDTH / DISPLAY_COLUMN_NUMBER)
     end
@@ -31,4 +31,5 @@ end
 
 path = ARGV[0] || '.'
 files = get_files(path)
-display_files(tranpose_files(files))
+files_sliced_by_rows = transpose_files(files)
+display_files(files_sliced_by_rows)
